647314960cd2ec54e53aa566cb36949667e060bd
Jacek Miszczuk <jacekjrm@gmail.com>
2026-01-20 11:00:49 +0100

    fixed logs

--- src/utils/logger-shim.ts
+++ src/utils/logger-shim.ts
@@ -3,16 +3,16 @@
 const disableInProduction = Boolean(import.meta.env?.PROD);
 const forceDisableAll = true; // set to true to always disable logs
 
-if (typeof window !== 'undefined' && (disableInProduction || forceDisableAll)) {
-  const methods: Array<keyof Console> = ['log', 'debug', 'info', 'warn', 'error'];
-  for (const m of methods) {
-    try {
-      // @ts-expect-error - assigning to console methods
-      console[m] = () => {};
-    } catch (_) {
-      // ignore
-    }
-  }
-}
+// if (typeof window !== 'undefined' && (disableInProduction || forceDisableAll)) {
+//   const methods: Array<keyof Console> = ['log', 'debug', 'info', 'warn', 'error'];
+//   for (const m of methods) {
+//     try {
+//       // @ts-expect-error - assigning to console methods
+//       console[m] = () => {};
+//     } catch (_) {
+//       // ignore
+//     }
+//   }
+// }
 
 export {};
