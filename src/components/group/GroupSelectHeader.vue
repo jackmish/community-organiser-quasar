37bf39217f970aad8e95bd52a5143bfb3168d272
jacek_m <j.miszczuk@moonbite.pl>
2026-03-10 21:15:26 +0100

    group shortcut border

--- src/components/group/GroupSelectHeader.vue
+++ src/components/group/GroupSelectHeader.vue
@@ -132,7 +132,9 @@
           @click.stop.prevent="onShortcutClick(g)"
           :style="`background-color: ${g.color || 'transparent'} !important; color: ${
             g.textColor || g.text_color || (g.color ? '#ffffff' : 'inherit')
-          }; padding: 4px 8px; min-height: 28px; display: inline-flex; align-items: center; gap: 8px; background-image: none !important; border-color: transparent !important; box-shadow: none !important;`"
+          }; border:1px solid ${
+            g.textColor
+          }; padding: 4px 8px; min-height: 28px; display: inline-flex; align-items: center; gap: 8px; background-image: none !important;  box-shadow: none !important;`"
         >
           <q-icon
             :name="g.icon || 'folder_open'"
